b39579029d50081964509733bcca7d8e181a630d
Fallen_Breath <nellaforax@outlook.com>
2026-02-08 00:49:20 +0800

    warn if import cy fail

--- fastcdc2020/__init__.py
+++ fastcdc2020/__init__.py
@@ -17,3 +17,5 @@ try:
 	from fastcdc2020.cy import FastCDC
 except ImportError:
 	from fastcdc2020.py import FastCDC
+	import warnings
+	warnings.warn('Failed to import fastcdc2020 cython extension, fallback to pure python implementation with is a lot slower.')
